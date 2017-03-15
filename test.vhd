19c19fa0b4521d05838ed0ec7328007cca2ac0c7
Robert Ou <rqou@robertou.com>
2017-03-15 00:42:44 -0700

    Mark done grammar

--- test.vhd
+++ test.vhd
@@ -1,5 +1,5 @@
 process
     package aaa is new bbb;
-    package ccc is new ddd generic map(ggg(eee) => hhh(fff, ggg, bar range baz'qux)(open));
+    package ccc is new ddd generic map(ggg(eee) => hhh(fff, ggg, bar range baz'qux)(0 to 2, 1 to 3));
 begin
 end process;
