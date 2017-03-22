aea91ded1794df93cbb65760ab682333294c4c9e
Robert Ou <rqou@robertou.com>
2017-03-22 16:40:43 -0700

    Initial work on semantic analysis

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 bison -v -d vhdl_parser.y
 flex vhdl_lexer.l
-g++ -DVHDL_PARSER_DEMO_MODE -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp -o vhdl_parser
+g++ -DVHDL_ANALYSIS_DEMO_MODE -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp vhdl_analysis_design_db.cpp -o vhdl_parser
