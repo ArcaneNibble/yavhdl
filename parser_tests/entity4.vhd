3afac71599cebed4ad1721d71ef6fe2119eb4d72
Robert Ou <rqou@robertou.com>
2017-03-24 18:02:11 -0700

    Add expect-fail tests

---
+++ parser_tests/entity4.vhd
@@ -0,0 +1,1 @@
+entity test begin end;
--- test.py
+++ test.py
@@ -21,8 +21,13 @@ def do_parser_tests():
         name, ext = os.path.basename(f).rsplit(".", 1)
         vhd_name = "parser_tests/" + name + ".vhd"
         json_name = "parser_tests/" + name + ".json"
-        if os.path.isfile(vhd_name) and os.path.isfile(json_name):
-            this_test = (vhd_name, json_name, name)
+        fail_name = "parser_tests/" + name + ".fail"
+        if (os.path.isfile(vhd_name) and
+            (os.path.isfile(json_name) or os.path.isfile(fail_name))):
+            if os.path.isfile(json_name):
+                this_test = (vhd_name, json_name, name)
+            else:
+                this_test = (vhd_name, None, name)
             if name not in test_files_set:
                 print("Found test \"" + name + "\"")
                 test_files_real.append(this_test)
@@ -33,62 +38,79 @@ def do_parser_tests():
     # Run each test
     failures = False
     for vhd_file, json_file, base_name in test_files_real:
-        print(base_name + ": ", end='')
-
-        # Load reference
-        with open(json_file, 'r') as inf:
-            reference = json.load(inf)
+        if json_file:
+            print(base_name + ": ", end='')
+        else:
+            print(base_name + " (expect fail): ", end='')
 
         # Run parser
         subp = subprocess.run(['./vhdl_parser', vhd_file],
                               stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE)
 
-        if subp.returncode != 0:
-            failures = True
-            print("\x1b[31m✗")
-            print("Executing parser failed!\x1b[0m")
-            print("\x1b[33m----- stdout -----\x1b[0m")
-            sys.stdout.buffer.write(subp.stdout)
-            print("\x1b[33m----- stderr -----\x1b[0m")
-            sys.stdout.buffer.write(subp.stderr)
-            continue
+        if json_file:
+            # Load reference
+            with open(json_file, 'r') as inf:
+                reference = json.load(inf)
 
-        # Load parser result
-        try:
-            prog_output = json.loads(subp.stdout.decode('ascii'))
-        except:
-            failures = True
-            print("\x1b[31m✗")
-            print("Bad parser output!\x1b[0m")
-            print("\x1b[33m----- stdout -----\x1b[0m")
-            sys.stdout.buffer.write(subp.stdout)
-            print("\x1b[33m----- stderr -----\x1b[0m")
-            sys.stdout.buffer.write(subp.stderr)
-            continue
+            if subp.returncode != 0:
+                failures = True
+                print("\x1b[31m✗")
+                print("Executing parser failed!\x1b[0m")
+                print("\x1b[33m----- stdout -----\x1b[0m")
+                sys.stdout.buffer.write(subp.stdout)
+                print("\x1b[33m----- stderr -----\x1b[0m")
+                sys.stdout.buffer.write(subp.stderr)
+                continue
 
-        # Compare
-        if prog_output != reference:
-            failures = True
-            print("\x1b[31m✗")
-            print("Test output mismatch!\x1b[0m")
-            # Re-encode in sorted order for proper diffing
-            ref_json = json.dumps(reference, indent=4,
-                                  separators=(',', ': '), sort_keys=True)
-            out_json = json.dumps(prog_output, indent=4,
-                                  separators=(',', ': '), sort_keys=True)
-            print("\x1b[33m----- expected -----\x1b[0m")
-            print(ref_json)
-            print("\x1b[33m----- actual -----\x1b[0m")
-            print(out_json)
-            print("\x1b[33m----- diff -----\x1b[0m")
-            udiff = difflib.unified_diff(ref_json.split('\n'),
-                                         out_json.split('\n'),
-                                         fromfile="expected_output",
-                                         tofile="parser_output",
-                                         lineterm='')
-            print('\n'.join(udiff))
+            # Load parser result
+            try:
+                prog_output = json.loads(subp.stdout.decode('ascii'))
+            except:
+                failures = True
+                print("\x1b[31m✗")
+                print("Bad parser output!\x1b[0m")
+                print("\x1b[33m----- stdout -----\x1b[0m")
+                sys.stdout.buffer.write(subp.stdout)
+                print("\x1b[33m----- stderr -----\x1b[0m")
+                sys.stdout.buffer.write(subp.stderr)
+                continue
+
+            # Compare
+            if prog_output != reference:
+                failures = True
+                print("\x1b[31m✗")
+                print("Test output mismatch!\x1b[0m")
+                # Re-encode in sorted order for proper diffing
+                ref_json = json.dumps(reference, indent=4,
+                                      separators=(',', ': '), sort_keys=True)
+                out_json = json.dumps(prog_output, indent=4,
+                                      separators=(',', ': '), sort_keys=True)
+                print("\x1b[33m----- expected -----\x1b[0m")
+                print(ref_json)
+                print("\x1b[33m----- actual -----\x1b[0m")
+                print(out_json)
+                print("\x1b[33m----- diff -----\x1b[0m")
+                udiff = difflib.unified_diff(ref_json.split('\n'),
+                                             out_json.split('\n'),
+                                             fromfile="expected_output",
+                                             tofile="parser_output",
+                                             lineterm='')
+                print('\n'.join(udiff))
+            else:
+                print("\x1b[32m✓\x1b[0m")
         else:
+            # Expect failure
+            if subp.returncode == 0:
+                failures = True
+                print("\x1b[31m✗")
+                print("Executing parser succeeded when it should not!\x1b[0m")
+                print("\x1b[33m----- stdout -----\x1b[0m")
+                sys.stdout.buffer.write(subp.stdout)
+                print("\x1b[33m----- stderr -----\x1b[0m")
+                sys.stdout.buffer.write(subp.stderr)
+                continue
+
             print("\x1b[32m✓\x1b[0m")
 
     return failures
