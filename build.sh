ad88869b48878099578038dcf55824ad6148bd52
Robert Ou <rqou@robertou.com>
2017-03-25 20:02:35 -0700

    Add a class for identifiers

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 bison -v -d vhdl_parser.y
 flex vhdl_lexer.l
-g++ -DVHDL_PARSER_DEMO_MODE -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp vhdl_analysis_design_db.cpp util.cpp -o vhdl_parser
-g++ -DVHDL_ANALYSIS_DEMO_MODE -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp vhdl_analysis_design_db.cpp util.cpp -o vhdl_analyser
+g++ -DVHDL_PARSER_DEMO_MODE -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp vhdl_analysis_design_db.cpp util.cpp vhdl_analysis_identifier.cpp -o vhdl_parser
+g++ -DVHDL_ANALYSIS_DEMO_MODE -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp vhdl_analysis_design_db.cpp util.cpp vhdl_analysis_identifier.cpp -o vhdl_analyser
 
 # FIXME: WTF?
-g++ -c -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp vhdl_analysis_design_db.cpp util.cpp
-ld -r vhdl_parser.tab.o lex.frontend_vhdl_yy.o vhdl_parse_tree.o vhdl_parser_glue.o vhdl_analysis_design_db.o util.o -o vhdl_analyser_bits.o
+g++ -c -ggdb3 -Wall vhdl_parser.tab.c lex.frontend_vhdl_yy.c vhdl_parse_tree.cpp vhdl_parser_glue.cpp vhdl_analysis_design_db.cpp util.cpp vhdl_analysis_identifier.cpp
+ld -r vhdl_parser.tab.o lex.frontend_vhdl_yy.o vhdl_parse_tree.o vhdl_parser_glue.o vhdl_analysis_design_db.o util.o vhdl_analysis_identifier.o -o vhdl_analyser_bits.o
---
+++ vhdl_analysis_identifier.h
@@ -0,0 +1,56 @@
+/*
+Copyright (c) 2016-2017, Robert Ou <rqou@robertou.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef VHDL_ANALYSIS_IDENTIFIER_H
+#define VHDL_ANALYSIS_IDENTIFIER_H
+
+#include <string>
+
+namespace YaVHDL::Analyser
+{
+
+class Identifier {
+public:
+    // Original Latin-1 name, no changes made
+    std::string orig_name;
+    // Lowercased name in the case of basic identifiers, otherwise the same
+    // as orig_name
+    std::string canonical_name;
+    // Pretty name suitable for printing. UTF-8
+    std::string pretty_name;
+    bool is_extended_id;
+
+    static Identifier *FromLatin1(const char *name, bool is_extended_id);
+    static Identifier *FromUTF8(const char *name, bool is_extended_id);
+
+private:
+    Identifier();
+    static Identifier *_IdentifierInternalCreate(
+        std::string name, bool is_extended_id);
+};
+
+}
+
+#endif
